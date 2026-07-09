cd4bc77fe07eceb2d075cee4497e17d2895cc280
Abdullah <abdullah.alnahhal@gmail.com>
2026-07-09 15:52:00 +0300

    set publish migrations

--- src/Providers/FieldFactoryServiceProvider.php
+++ src/Providers/FieldFactoryServiceProvider.php
@@ -19,6 +19,10 @@ class FieldFactoryServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->publishesMigrations([
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+        ]);
+
         // Load migrations, routes, and views
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/../Migrations');
